7ed170ccadcb41cda45e9be60714ade9ec24919c
Gabriel Souto <gabriel.souto@academico.ifpb.edu.br>
2023-10-09 20:50:18 -0300

    feat: api users v1

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,20 @@
+class ApplicationController < ActionController::API
+  #JSON return for 404 code
+  def not_found
+    render json: { error: 'not_found' }
+  end
+
+  #User validation to accept request
+  def authorize_request
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+    begin
+      @decoded = JsonWebToken.decode(header)
+      @current_user = User.find(@decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
+  end
+end

5bf76f7c8c5fac7f80831d11b5d4742afd6b1545
Paulo Risucci <paulo.risucci@academico.ifpb.edu.br>
2023-10-17 12:06:06 -0300

    Ajuste API Bicicletas

--- client-service/src/main/java/br/edu/ifpb/pdist/controller/BicicletaController.java
+++ client-service/src/main/java/br/edu/ifpb/pdist/controller/BicicletaController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.ALL_VALUE}, produces = MediaType.APPLICATION_JSON_VALUE)
 public class BicicletaController {
 
     private BicicletaService bicicletaService;
